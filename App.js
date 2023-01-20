df86f8a1da013a4adc1918665b47ca4cba920953
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2023-01-20 10:28:24 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button, Image, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 export default function ImagePickerExample() {
   const [status, requestPermission] = ImagePicker.useCameraPermissions();
+  const [photo, setPhoto] = useState();
   useEffect(() => {
     async function verPermissoes() {
       const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
@@ -22,12 +23,16 @@ export default function ImagePickerExample() {
     });
 
     console.log(imagem);
+    setPhoto(imagem.assets[0].uri);
   };
 
   return (
     <>
       <StatusBar />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        {photo && (
+          <Image source={{ uri: photo }} style={{ width: 300, height: 200 }} />
+        )}
         <Button title="acessar câmera" onPress={acessaCamera} />
       </View>
     </>
